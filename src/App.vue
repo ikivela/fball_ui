fb3d743f8822ddf63bab9607c433fc6dc3e4e20c
Ilkka Kivelä <ilkka.v.kivela@jyu.fi>
2021-11-17 11:02:46 +0200

    Working version

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,104 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container fluid>
+      <b-navbar type="light" variant="light">
+        <b-nav-form>
+          <b-input-group prepend="">
+            <b-form-input
+              id="filter-input"
+              v-model="filter"
+              type="search"
+              filter-debounce="1000"
+            ></b-form-input>
+          </b-input-group>
+          <b-span style="margin-left: 1em">
+            Nibacos otteluita: {{ this.games.length }}</b-span
+          >
+        </b-nav-form>
+      </b-navbar>
+
+      <b-table
+        responsive="sm"
+        small
+        :items="games"
+        :fields="fields"
+        @filtered="onFiltered"
+        :filter="filter"
+        :filter-included-fields="filterOn"
+      ></b-table>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import { DateTime } from "luxon";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      show: true,
+      items: [],
+      games: [],
+      filter: null,
+      filterOn: [],
+      totalRows: 1,
+      fields: [
+        "GameDate",
+        "GameTime",
+        "HomeTeamName",
+        "AwayTeamName",
+        "RinkName",
+        "Result",
+        "group",
+        "class",
+      ],
+    };
+  },
+  created() {
+    document.title = "Nibacos ottelut";
+  },
+  computed: {},
+  async mounted() {
+    /*let search = window.location.search.substring(1);
+    if (search.length > 0) {
+      search = search.split("=")[1].replace("%20", " ");
+      this.filter = search == "" ? null : search;
+      console.log("filter = ", this.filter);
+    }
+    */
+    let _games = await this.getGames();
+    this.games = _games.sort((a, b) => {
+      let a_date = DateTime.fromISO(a.GameDate + "T" + a.GameTime).toMillis();
+      let b_date = DateTime.fromISO(b.GameDate + "T" + b.GameTime).toMillis();
+
+      return a_date > b_date ? 1 : -1;
+    });
+    this.games.forEach((game) =>
+      console.log(
+        game.GameDate,
+        game.GameTime,
+        game.HomeTeamName,
+        game.AwayTeamName
+      )
+    );
+  },
+  methods: {
+    async getGames() {
+      let response = await axios.get("http://localhost:8000/games");
+      return response.data;
+    },
+    onFiltered(filteredItems) {
+      // Trigger pagination to update the number of buttons/pages due to filtering
+      this.totalRows = filteredItems.length;
+      this.currentPage = 1;
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "~@/assets/scss/vendors/bootstrap-vue/index";
 </style>
