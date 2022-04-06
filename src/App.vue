acab5de9dc9cd495d8406155adfbdcd8bb2376cb
Ilkka Kivelä <ilkka.v.kivela@jyu.fi>
2022-04-06 10:05:40 +0300

    Added stats

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,77 @@
 <template>
   <div id="app">
-    <b-container fluid>
-      <b-navbar type="light" variant="light">
-        <b-navbar-brand
-          ><img
-            height="36"
-            src="https://static.jopox.fi/nibacos/imagebank/40875_huge.png"
-            alt="logo"
-        /></b-navbar-brand>
+    <b-navbar sticky toggleable="lg" type="light" variant="light">
+      <b-navbar-brand
+        ><img
+          height="36"
+          src="https://static.jopox.fi/nibacos/imagebank/40875_huge.png"
+          alt="logo"
+        />
+        Nibacos
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item
+            @click="(showGamesPage = true), (showStatsPage = false)"
+            href="#"
+            >Ottelut</b-nav-item
+          >
+          <b-nav-item
+            @click="(showGamesPage = false), (showStatsPage = true), setStats()"
+            href="#"
+            >Tilastot
+          </b-nav-item>
+        </b-navbar-nav>
         <b-nav-form>
-          <b-input-group prepend="">
-            <b-form-input
-              id="filter-input"
-              v-model="filter"
-              type="search"
-              filter-debounce="1000"
-              placeholder="Etsi.."
-            ></b-form-input>
-            <b-form-checkbox
-              id="showPast"
-              v-model="showPastValues"
-              value="true"
-              unchecked-value="false"
-            >
-              Näytä menneet
-            </b-form-checkbox>
-          </b-input-group>
-
-          <b-form-select
-            style="margin-left: 1em"
-            v-model="selectedSeason"
-            :options="this.seasons"
-            v-on:change="updateData()"
-          ></b-form-select>
-
-          <b-badge style="margin-left: 1em">
-            Ottelut: {{ this.games.length }}
-          </b-badge>
+          <b-form-input
+            id="filter-input"
+            v-model="filter"
+            type="search"
+            filter-debounce="1000"
+            size="sm"
+            class="mr-sm-2"
+            placeholder="Etsi"
+          ></b-form-input>
         </b-nav-form>
-      </b-navbar>
+      </b-collapse>
+    </b-navbar>
 
+    <b-container fluid v-if="showGamesPage" id="games">
+      <b-nav-form>
+        <b-input-group prepend="">
+          <b-form-checkbox
+            id="showPast"
+            v-model="showPastValues"
+            value="true"
+            unchecked-value="false"
+          >
+            Näytä menneet
+          </b-form-checkbox>
+        </b-input-group>
+        <b-form-select
+          style="margin-left: 1em"
+          v-model="selectedSeason"
+          :options="this.seasons"
+          v-on:change="updateData()"
+        ></b-form-select>
+
+        <b-badge style="margin-left: 1em">
+          Ottelut: {{ this.games.length }}
+        </b-badge>
+        <b-form-select
+          v-model="selectedClass"
+          v-on:change="getSelectedClass"
+          :options="classes"
+        ></b-form-select>
+      </b-nav-form>
       <b-table
-        responsive="sm"
-        small
+        responsive="lg"
         :items="games"
         :fields="fields"
         @filtered="onFiltered"
         :filter="filter"
         :filter-included-fields="filterOn"
-        sticky-header="true"
-        style="max-height: 800px"
       >
         <template #cell(Result)="data">
           <a
@@ -65,34 +87,43 @@
           ></template
         >
       </b-table>
-      <b-modal ok-only v-model="showGameStat">
-        <b-spinner v-if="this.loading" label="">Ladataan...</b-spinner>
-
-        <div class="d-block text-center">
-          <b>{{ this.currentGame }}</b>
-        </div>
-        <ul v-if="gameStats.length > 0">
-          <li v-for="stat in gameStats" v-bind:key="stat.time">
-            {{ stat.time }}
-            {{ stat.event == "goal" ? stat.result : stat.penalty_time }}
-            {{ stat.team }}
-            {{
-              stat.yv_av
-                ? stat.yv_av == "RL0"
-                  ? "Rangaistusl. (epäonnistunut)"
-                  : stat.yv_av
-                : ""
-            }}
-            {{
-              stat.event == "goal"
-                ? stat.scorer + (stat.assist ? " (" + stat.assist + ")" : "")
-                : ""
-            }}
-            {{ stat.event == "penalty" ? stat.reason : "" }}
-          </li>
-        </ul>
-      </b-modal>
     </b-container>
+
+    <b-container v-if="showStatsPage" id="stats">
+      <b-form-select
+        v-model="selectedClass"
+        v-on:change="getSelectedClass"
+        :options="classes"
+      ></b-form-select>
+      <b-table small :items="this.statsData[0].stats"> </b-table>
+    </b-container>
+    <b-modal ok-only v-model="showGameStat">
+      <b-spinner v-if="this.loading" label="">Ladataan...</b-spinner>
+
+      <div class="d-block text-center">
+        <b>{{ this.currentGame }}</b>
+      </div>
+      <ul v-if="gameStats.length > 0">
+        <li v-for="stat in gameStats" v-bind:key="stat.time">
+          {{ stat.time }}
+          {{ stat.event == "goal" ? stat.result : stat.penalty_time }}
+          {{ stat.team }}
+          {{
+            stat.yv_av
+              ? stat.yv_av == "RL0"
+                ? "Rangaistusl. (epäonnistunut)"
+                : stat.yv_av
+              : ""
+          }}
+          {{
+            stat.event == "goal"
+              ? stat.scorer + (stat.assist ? " (" + stat.assist + ")" : "")
+              : ""
+          }}
+          {{ stat.event == "penalty" ? stat.player + " " + stat.reason : "" }}
+        </li>
+      </ul>
+    </b-modal>
   </div>
 </template>
 
@@ -112,11 +143,18 @@ export default {
       updated: "",
       options: [],
       seasons: [],
+      showGamesPage: false,
+      showStatsPage: false,
+      showGameStats: false,
       showGameStat: false,
       gameStats: "",
+      seasonStats: "",
       loading: false,
       allGames: [],
+      statsData: "",
       selectedSeason: null,
+      selectedClass: "",
+      showStats: false,
       currentGame: "",
       items: [],
       filter: null,
@@ -143,6 +181,8 @@ export default {
     // If season is 2021-2022, the currentSeason has to be 2022
     // Season is changed to new season after 1st of August
     this.seasons = await this.getSeasons();
+    this.seasonStats = await this.getStats();
+    //console.log(this.seasonStats);
     await this.updateData();
   },
   computed: {
@@ -153,6 +193,19 @@ export default {
       get: function () {
         return this.show;
       },
+    },
+
+    classes() {
+      let classes = this.allGames
+        .map((x) => {
+          return x.class;
+        })
+        .filter((_value, _index, _self) => {
+          return _self.indexOf(_value) === _index;
+        })
+        .sort();
+      classes.unshift("Näytä kaikki");
+      return classes;
     },
     games() {
       console.log(
@@ -178,11 +231,17 @@ export default {
     },
   },
   methods: {
+    getSelectedClass(_class) {
+      console.log("get class", _class);
+      _class = _class !== "Näytä kaikki" ? _class : "";
+      this.filter = _class;
+      if (this.showStats) this.setStats(_class);
+    },
     async updateData() {
       console.log("selected season [%s]", this.selectedSeason);
       this.allGames = await this.getGames(this.selectedSeason);
       this.showPastValues = "true";
-      console.log(this.allGames.length);
+      console.log("allGames.length:", this.allGames.length);
       this.allGames = this.allGames.sort((a, b) => {
         let a_date = DateTime.fromISO(a.GameDate + "T" + a.GameTime).toMillis();
         let b_date = DateTime.fromISO(b.GameDate + "T" + b.GameTime).toMillis();
@@ -211,6 +270,19 @@ export default {
         });
 
       return _id;
+    },
+    setStats(_class, toggle) {
+      this.showStats = toggle ? toggle : true;
+      _class = _class ? _class : this.classes[1];
+      this.selectedClass = _class;
+      console.log("setStat %s %s", _class, this.selectedSeason);
+      this.statsData = this.seasonStats.filter(
+        (x) => x.season == this.selectedSeason && x.class == _class
+      );
+    },
+    async getStats() {
+      let res = await axios.get(`${this.baseurl}/seasonstats`);
+      return res.data;
     },
     async getSeasons() {
       let res = await axios.get(`${this.baseurl}/seasons`);
