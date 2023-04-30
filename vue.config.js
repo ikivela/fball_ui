ee871e217b437376984c1c9c60039705617df794
Ilkka Kivelä <ilkka.kivela@gmail.com>
2023-04-30 21:30:43 +0300

    update production path

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
       },
     },
   },
-  publicPath: process.env.NODE_ENV === "production" ? "/nbcs/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
       args[0].title = process.env.VUE_APP_SITE_TITLE
